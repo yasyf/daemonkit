13e525eda5e18905375b2a820647cc9c3f0375ff
Yasyf Mohamedali <yasyfm@gmail.com>
2026-07-18 02:57:43 +1200

    fix: silence revive on the non-darwin ShortVersion stub; require Go 1.26.3 (stdlib GO-2026-4971)

--- bundle/plist_other.go
+++ bundle/plist_other.go
@@ -5,6 +5,6 @@ package bundle
 import "errors"
 
 // ShortVersion reports that reading an .app Info.plist is darwin-only.
-func ShortVersion(appPath string) (string, error) {
+func ShortVersion(_ string) (string, error) {
 	return "", errors.New("bundle: reading Info.plist is only supported on darwin")
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/yasyf/daemonkit
 
-go 1.26.2
+go 1.26.3
 
 require github.com/ebitengine/purego v0.10.1
 
